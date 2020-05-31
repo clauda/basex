28e008ed7a22a0fef823e2c1de6271e96e97bbc3
Claudia Farias <farias.claudiac@gmail.com>
2020-05-31 17:30:21 -0300

    production stuff

--- mix.exs
+++ mix.exs
@@ -43,7 +43,6 @@ defmodule Basex.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
